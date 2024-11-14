e3ed6ee8aded6d9898d286671620cebe8211dc09
Gwannon <monclus.jorge@gmail.com>
2024-11-14 20:03:54 +0100

    Refactorizar generador docs

--- pdf.sh
+++ pdf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-php ./tools/generateDocs.php > ./metas.txt
+php ./tools/generateDocs.php
 chromium --no-sandbox --headless --gpu --no-pdf-header-footer --print-to-pdf=./temp.pdf ./index.html
 pdftk 'temp.pdf' update_info_utf8 'metas.txt' output 'bosque-Sh3rW00d.pdf'
 rm metas.txt
--- tools/generateDocs.php
+++ tools/generateDocs.php
@@ -1,7 +1,6 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
-
 include(__DIR__ ."/config.php");
 
 //Generamos el HTML
@@ -22,22 +21,17 @@ $html = str_replace("</h1>", "</h1>\n<div class=\"saltopagina\"></div><div class
 $html = str_replace("<div class='columns'>\n</div>", "", $html); 
 $html = str_replace("<table>", "<div><table>", $html); 
 $html = str_replace("</table>", "</table></div>", $html); 
-
 $html = str_replace("<p><strong>Semilla de aventura:</strong>", "<p class='seed'><strong>Semilla de aventura:</strong>", $html); 
 
+//Generamos MEtas
+$metas = "InfoKey: Subject\n";
+$metas .= "InfoValue: ".$tags['DESCRIPTION']." Versión ".$tags['VERSION']."\n\n";
+$metas .= "InfoKey: Author\n";
+$metas .= "InfoValue: ".$tags['AUTHOR']."\n\n";
+$metas .= "InfoKey: Keywords\n";
+$metas .= "InfoValue: ".$tags['KEYWORDS']."\n\n";
 
-file_put_contents(__DIR__ . "/../index.html", $html);
-
-echo "InfoKey: Subject\n";
-echo "InfoValue: ".$tags['DESCRIPTION']." Versión ".$tags['VERSION']."\n\n";
-echo "InfoKey: Author\n";
-echo "InfoValue: ".$tags['AUTHOR']."\n\n";
-echo "InfoKey: Keywords\n";
-echo "InfoValue: ".$tags['KEYWORDS']."\n\n";
-
-
-
-/* Generamos indice */
+/* Generamos indice del PDF */
 /* -------------------------------------------------------------- */
 $doc = new DOMDocument();
 $internalErrors = libxml_use_internal_errors(true);
@@ -45,105 +39,79 @@ $doc->loadHTMLFile("index.html");
 $body = $doc->getElementsByTagName('body');
 $body = $body->item(0);
 $json = [];
-
-$html = explode("\n", removeHtmlComments($doc->savehtml($body)));
-
 $lines = [];
-foreach($html as $line) {
+
+foreach(explode("\n", removeHtmlComments($doc->savehtml($body))) as $line) {
   $line = cleanLine($line);
   if(preg_match("/(<h1>|<h2|<h3|<h4|saltopagina)/", $line)) $lines[] = $line;
 }
-$counter = 1;
-echo "BookmarkBegin\n";
-echo "BookmarkTitle: Portada\n";
-echo "BookmarkLevel: 1\n";
-echo "BookmarkPageNumber: {$counter}\n";
-$json[] = ["title" => "Portada","page" => $counter];
 
-$counter = 3;
-echo "BookmarkBegin\n";
-echo "BookmarkTitle: Licencia de uso\n";
-echo "BookmarkLevel: 1\n";
-echo "BookmarkPageNumber: {$counter}\n";
-$json[] = ["title" => "Licencia de uso","page" => $counter];
+$metas .= bookMark("Portada", 1, 1);
+$json[] = ["title" => "Portada","page" => 1];
 
-$counter = 4;
-echo "BookmarkBegin\n";
-echo "BookmarkTitle: Índice\n";
-echo "BookmarkLevel: 1\n";
-echo "BookmarkPageNumber: {$counter}\n";
-$json[] = ["title" => "Índice","page" => $counter];
+$metas .= bookMark("Licencia de uso", 1, 3);
+$json[] = ["title" => "Licencia de uso","page" => 3];
+
+$metas .= bookMark("Índice", 1, 4);
+$json[] = ["title" => "Índice","page" => 4];
 
 $counter = 1;
 foreach($lines as $line) {
   if(preg_match("/(<h1>)/", $line)) {
-
-    $line = str_replace(" <strong>" , "____", $line);
-    $line = str_replace("</strong>" , "", $line);
+    $line = str_replace("</strong>" , "", $line = str_replace(" <strong>" , "____", $line));
     $line = strip_tags($line);
     $temp = explode("____", $line);
-    echo "BookmarkBegin\n";
-    echo "BookmarkTitle: ".$temp[1]."\n";
-    echo "BookmarkLevel: 1\n";
-    echo "BookmarkPageNumber: {$counter}\n";
+    $metas .= bookMark($temp[1], 1, $counter);
     $json[] = ["title" => $temp[1],"page" => $counter, "tag" => "H1"];
   } else if(preg_match("/(<h2>)/", $line)) {
     $line = strip_tags($line);
-    echo "BookmarkBegin\n";
-    echo "BookmarkTitle: {$line}\n";
-    echo "BookmarkLevel: 2\n";
-    echo "BookmarkPageNumber: {$counter}\n";
+    $metas .= bookMark($line, 2, $counter);
     $json[] = ["title" => $line,"page" => $counter, "tag" => "H2"];
   } else if(preg_match("/(<h3>)/", $line)) {
     $line = strip_tags($line);
-    echo "BookmarkBegin\n";
-    echo "BookmarkTitle: {$line}\n";
-    echo "BookmarkLevel: 3\n";
-    echo "BookmarkPageNumber: {$counter}\n";
+    $metas .= bookMark($line, 3, $counter);
     $json[] = ["title" => $line,"page" => $counter, "tag" => "H3"];
   } else if(preg_match("/(<h4>)/", $line)) {
     $line = strip_tags($line);
-    echo "BookmarkBegin\n";
-    echo "BookmarkTitle: {$line}\n";
-    echo "BookmarkLevel: 4\n";
-    echo "BookmarkPageNumber: {$counter}\n";
+    $metas .= bookMark($line, 4, $counter);
     $json[] = ["title" => $line,"page" => $counter, "tag" => "H4"];
   } else if(preg_match("/saltopagina/", $line)) {
     $counter++;
   }
 }
 
-echo "BookmarkBegin\n";
-echo "BookmarkTitle: Contraportada\n";
-echo "BookmarkLevel: 1\n";
-echo "BookmarkPageNumber: {$counter}\n";
-$json[] = ["title" => "Contraportada","page" => $counter];
+$metas .= bookMark("Contraportada", 1, $counter);
+$json[] = ["title" => "Contraportada", "page" => $counter];
 
-$html = file_get_contents(__DIR__ . "/../index.html");
-//INDICE
+//Generamos el Índice HTML
 $indice = "";
 foreach ($json as $item) {
   if(isset($item['tag']) && in_array($item['tag'], ['H1', 'H2'])) $indice .= '<a href="#anchor' . $item['page'] . '" class="like' . $item['tag'] . '"><span>' . $item['page'] . '</span>' . $item['title'] . '</a>';
 }
 $html = str_replace("|INDICE|", $indice, $html);
 
-
-
 $counter = 2;
 $html = preg_replace_callback("/\"saltopagina\"/", function($matches) {
   global $counter;
-
   $matches[0] = '"saltopagina" id="anchor'.$counter.'"';
   $counter++;
   return $matches[0];
 }, $html);
 
-
 file_put_contents(__DIR__ . "/../index.html", $html);
+file_put_contents(__DIR__ . "/../metas.txt", $metas);
 
 function cleanLine($line) {
   $line = str_replace(array("  ", "   ", "\t", "\n", "\r"), "", $line);
   return $line;
+}
+
+function bookMark($title, $level, $counter) {
+  $metas = "BookmarkBegin\n";
+  $metas .= "BookmarkTitle: {$title}\n";
+  $metas .= "BookmarkLevel: {$level}\n";
+  $metas .= "BookmarkPageNumber: {$counter}\n";
+  return $metas;
 }
 
 function removeHtmlComments($content = '') {
